f8228b294075a287e88453c37c7e44f2189b15e7
Shreyas Udupa <shreyasudupas@gmail.com>
2021-10-18 22:57:55 +0530

    Cart-Icon change basecomponent

--- src/app/UserComponent/user-dashboard/user-dashboard.component.ts
+++ src/app/UserComponent/user-dashboard/user-dashboard.component.ts
@@ -14,62 +14,65 @@ import { environment as env } from '../../../environments/environment';
   templateUrl: './user-dashboard.component.html',
   styleUrls: ['./user-dashboard.component.css']
 })
-export class UserDashboardComponent extends BaseComponent<UserInfo> implements OnInit {
+export class UserDashboardComponent implements OnInit {
  
   UserProfile:UserInfo;
   MenuItems: MenuItem[];
   CurrentUserRole:string;
   
 
-  constructor(private AuthService:AuthService,httpclient:HttpClient,
-    public _broadcastService:DataSharingService,
-    public _menuService:MenuService) {
-    super(_menuService,httpclient,_broadcastService);
+  constructor(
+    //private AuthService:AuthService,
+    //httpclient:HttpClient,
+    //public _broadcastService:DataSharingService,
+    //public _menuService:MenuService
+    ) {
+    //super(_menuService,httpclient,_broadcastService);
    }
 
   ngOnInit(): void {
     
-    this.UserProfile = this.AuthService.getUserInformation();
+    //this.UserProfile = this.AuthService.getUserInformation();
 
     this.CurrentUserRole = "user";
 
 
       //fork join two  calls
-      let forkRequest = new ResourceServiceForkRequest();
-      let RequestResource1 = new RequestResource();
-      RequestResource1.requestUrl = env.userAPI+'GetOrUpdateUserDetails';
-      RequestResource1.httpMethod ='post';
-      RequestResource1.body = this.UserProfile;
-      let RequestResource2 = new RequestResource();
-      RequestResource2.requestUrl = env.cartInfoAPI+'GetUserBasketInfoFromCache';
-      RequestResource2.httpMethod ='get';
-      forkRequest.requestParamter = new Array<RequestResource>();
-      forkRequest.requestParamter.push(RequestResource1);
-      forkRequest.requestParamter.push(RequestResource2);
+      // let forkRequest = new ResourceServiceForkRequest();
+      // let RequestResource1 = new RequestResource();
+      // RequestResource1.requestUrl = env.userAPI+'GetOrUpdateUserDetails';
+      // RequestResource1.httpMethod ='post';
+      // RequestResource1.body = this.UserProfile;
+      // let RequestResource2 = new RequestResource();
+      // RequestResource2.requestUrl = env.cartInfoAPI+'GetUserBasketInfoFromCache';
+      // RequestResource2.httpMethod ='get';
+      // forkRequest.requestParamter = new Array<RequestResource>();
+      // forkRequest.requestParamter.push(RequestResource1);
+      // forkRequest.requestParamter.push(RequestResource2);
 
 
-      this.getForkItems(forkRequest).subscribe(([userProfileResponse,UserCartInformation])=>{
-        let UserProfileResponse = userProfileResponse;
-        let UserCartInfoResponse = JSON.parse(UserCartInformation);
-        // if(responseList.length>0){
-        // }
-        if(UserProfileResponse != null){
-          this.UserProfile.points = UserProfileResponse.points;
-          this.UserProfile.cartAmount = UserProfileResponse.cartAmount;
-          this.UserProfile.roleName = UserProfileResponse.roleName;
-        }
+      // this.getForkItems(forkRequest).subscribe(([userProfileResponse,UserCartInformation])=>{
+      //   let UserProfileResponse = userProfileResponse;
+      //   let UserCartInfoResponse = JSON.parse(UserCartInformation);
+      //   // if(responseList.length>0){
+      //   // }
+      //   if(UserProfileResponse != null){
+      //     this.UserProfile.points = UserProfileResponse.points;
+      //     this.UserProfile.cartAmount = UserProfileResponse.cartAmount;
+      //     this.UserProfile.roleName = UserProfileResponse.roleName;
+      //   }
 
-          //cart information
-        if(UserCartInfoResponse.Items != null){
-        let count =0;
-        if(UserCartInfoResponse.Items.length >0){
-            UserCartInfoResponse.Items.forEach((item:any) =>{
-                  count +=item.quantity;
-               });
-            }
-            this._broadcastService.updateCartCountWithvalue(count);
-          }
-      });
+      //     //cart information
+      //   if(UserCartInfoResponse.Items != null){
+      //   let count =0;
+      //   if(UserCartInfoResponse.Items.length >0){
+      //       UserCartInfoResponse.Items.forEach((item:any) =>{
+      //             count +=item.quantity;
+      //          });
+      //       }
+      //       this._broadcastService.updateCartCountWithvalue(count);
+      //     }
+      // });
 
       
 
